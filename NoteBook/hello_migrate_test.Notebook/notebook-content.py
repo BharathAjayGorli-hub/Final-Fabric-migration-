a328a00de026d89735a86d9a824a53e620e71d59
BharathAjayGorli-hub <superonezero10@gmail.com>
2025-09-04 17:22:05 -0700

    Committing 1 item from workspace 51ff8cf4-d80c-4ed4-ac48-9afdf978c965

---
+++ NoteBook/hello_migrate_test.Notebook/notebook-content.py
@@ -0,0 +1,11 @@
+# Fabric notebook source
+
+
+# MARKDOWN ********************
+
+# # Hello from a migrated notebook
+# This notebook is a tiny test file you can upload to ADLS/Blob and migrate to Microsoft Fabric via your Synapse pipeline.
+
+# CELL ********************
+
+print('✅ Hello from Fabric! This notebook was migrated successfully.')
